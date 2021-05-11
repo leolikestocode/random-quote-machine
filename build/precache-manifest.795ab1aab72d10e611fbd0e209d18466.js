e4fda5c0c48d0ca72721ca3b6bce71e959b83249
leolikestocode <leonardofpereira25@gmail.com>
2021-05-10 21:33:28 -0300

    Build files now on git

---
+++ build/precache-manifest.795ab1aab72d10e611fbd0e209d18466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f66ec1b1103e12fdc7a1683f96395e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2226b181be00a69e866",
+    "url": "/static/css/main.036c9d66.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "f2226b181be00a69e866",
+    "url": "/static/js/main.17578cea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
